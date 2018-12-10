e790b8f0f3d14e0ca88fec92d72b46b479d3cf71
hamdi <javs_shao@163.com>
2018-12-10 13:42:07 +0800

    代码修正_1

--- cookiespool/scheduler.py
+++ cookiespool/scheduler.py
@@ -21,7 +21,7 @@ class Scheduler(object):
                     time.sleep(cycle)
             except Exception as e:
                 print(e.args)
-
+    
     @staticmethod
     def generate_cookie(cycle=CYCLE):
         while True:
@@ -35,21 +35,21 @@ class Scheduler(object):
                     time.sleep(cycle)
             except Exception as e:
                 print(e.args)
-
+    
     @staticmethod
     def api():
         print('API接口开始运行')
         app.run(host=API_HOST, port=API_PORT)
-
+    
     def run(self):
         if API_PROCESS:
             api_process = Process(target=Scheduler.api)
             api_process.start()
-
+        
         if GENERATOR_PROCESS:
             generate_process = Process(target=Scheduler.generate_cookie)
             generate_process.start()
-
+        
         if VALID_PROCESS:
             valid_process = Process(target=Scheduler.valid_cookie)
             valid_process.start()
